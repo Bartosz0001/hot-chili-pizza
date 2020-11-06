fe97485da468f35463f1056c1b1883268ce6701c
Bartosz Iwiński <01bartosz0001@gmail.com>
2020-11-06 16:50:49 +0100

    Add styles for home page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,27 +1,39 @@
 import React from 'react';
 import Header from '../layout/Header';
+import './home.scss';
 
 const HomePage = () => (
     <div>
       <Header />
 
-      <section>
-        <h2>Best Pizza in the city</h2>
+      <section className='intro'>
+        <h2 className='section-title'>Best Pizza in the city</h2>
         <p>Since 2009 we delievered delicious pizza for our customers.</p>
         <p>Each day houndreds of people admire unique taste of our pizza, and quality of our service. We offer delicious
           pizza in our locale or delivery. We were the first to create really spicy pizza, using the hottest peppers 
           from the Mexico, for our most demanging clients.</p>
       </section>
 
-      <section>
-        <h2>Promotions</h2>
-        <h3>Two sauces for free!</h3>
-        <h3>Free delivery from $50!</h3>
-        <h3>Second pizza 20% cheaper!</h3>
+      <section className='promotions'>
+        <h2 className='section-title'>Promotions</h2>
+        <div className='wrapper'>
+          <div className='box'>
+            <p>Two sauces for free!</p>
+            <img src='https://storage.needpix.com/rsynced_images/sauce-4171459_1280.jpg' alt='sauces' />
+          </div>
+          <div className='box'>
+            <p>Free delivery from $50!</p>
+            <img src='https://images.pexels.com/photos/4393426/pexels-photo-4393426.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940' alt='delivery' />
+          </div>
+          <div className='box'>
+            <p>Second pizza 20% cheaper!</p>
+            <img src='https://images.pexels.com/photos/4109078/pexels-photo-4109078.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940' alt='two pizzas' />
+          </div>
+        </div>   
       </section>
 
-      <section>
-        <h2>Reviews</h2>
+      <section className='reviews'>
+        <h2 className='section-title'>Reviews</h2>
       </section>
     </div>
   );
