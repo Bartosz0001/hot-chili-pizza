f6a2f283ec777d77c090a17e0ac508c5e26048fb
Bartosz Iwiński <01bartosz0001@gmail.com>
2020-11-09 20:52:19 +0100

    Add Contact page

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,7 +1,33 @@
 import React from 'react';
+import Header from '../layout/Header';
+import Footer from '../layout/Footer';
 
 const Contact = () => (
     <div>
+      <Header />
+
+      <section className='contact'>
+        <div className='contact-wrapper'>
+          <div className='contact-box'>
+            <h2>Adress:</h2>
+            <h3>1600 Pennsylvania Avenue NW, Washington, DC</h3>
+          </div>
+          <div className='contact-box'>
+            <h2>Contact</h2>
+            <h3>+1 202-456-1111</h3>
+          </div>
+          <div className='contact-box'>
+            <h2>Open hours:</h2>
+            <h3>12:00 - 24:00</h3>
+          </div>
+        </div>
+
+        <div className='map'>
+          <h2>Map:</h2>
+        </div>
+      </section>
+
+      <Footer />
     </div>
   );
   
